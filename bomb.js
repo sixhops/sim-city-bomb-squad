05e4b3c47da3c1fb7e1a4d9da860830b85bdb87e
sixhops <sixhops@gmail.com>
2019-09-26 14:35:26 -0700

    Laid out html and css. Some js.

--- bomb.js
+++ bomb.js
@@ -1,1 +1,25 @@
 console.log('loaded!');
+
+// Variables
+const STARTING_TIME = 30;
+var remainingTime = 0;
+var gameOver = false;
+var countdown = null;
+var delay = null;
+
+var wireState = {
+  blue: false,
+  green: false,
+  red: false,
+  white: false,
+  yellow: false
+}
+
+var wiresToCut = [];
+
+// DOM References
+var timer = document.getElementById('timer');
+var wireBox = document.getElementById('wirebox');
+var resetButton = document.querySelector('button');
+// TODO: add audio elements
+
